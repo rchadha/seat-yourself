f1156e2dd99f88a453987207a3b1f12db184f645
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-23 15:16:04 -0400

    added reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
 
   validates :party_size, presence: true
+  validate :reservation_between_six_and_ten
   #validate :capacity_size
+
+  def reservation_between_six_and_ten
+  	if reserved_at.hour < 18 or reserved_at.hour > 20 
+  		errors[:base] << "Reservation hours has to be between 6 PM and 10 PM"
+  	end
+  	
+  end 
+
 end
