64a91a750616c5bca1deef71aa0d408fd7313b6c
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-23 19:18:33 -0400

    added google maps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518184119) do
+ActiveRecord::Schema.define(version: 20150523213527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150518184119) do
     t.string   "city"
     t.string   "postal_code"
     t.string   "phone_number"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: :cascade do |t|
