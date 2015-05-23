626a7a30557651a9b68c291ed8631c15ac5e0bdd
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-23 15:24:28 -0400

    added more validation for reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,17 @@
 class Reservation < ActiveRecord::Base
   belongs_to :restaurant
 
-  validates :party_size, presence: true
+  validates :party_size, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validate :reservation_between_six_and_ten
-  #validate :capacity_size
+  validate :capacity_size
+
+  def capacity_size
+  	existing_reservations = Reservation.where("restaurant_id = ? AND reserved_at > ? AND reserved_at < ?", self.restaurant_id, self.reserved_at.beginning_of_hour, self.reserved_at.end_of_hour)
+		total_seats_reserved = existing_reservations.sum(:party_size)
+		if party_size > (self.restaurant.capacity - total_seats_reserved)
+			errors[:base] << "Capacity Exceeded"
+		end
+  end 
 
   def reservation_between_six_and_ten
   	if reserved_at.hour < 18 or reserved_at.hour > 20 
