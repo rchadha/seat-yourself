a0bfed32bdc68a9d624f31adbc8d2ba5ea88301f
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-24 14:17:11 -0400

    wip

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,6 +1,7 @@
 class Cuisine < ActiveRecord::Base
   has_many :cuisine_restaurants, dependent: :destroy
-  #dependent destroy...if we destroy Cuisine, we should destroy from CusineRestaurants
+  #dependent destroy...if we destroy Cuisine, we should destroy relationship from
+  #CusineRestaurants
   has_many :Restaurants, :through => :cuisine_restaurants
 end
 
