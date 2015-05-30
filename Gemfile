d7b103546d4b86b1f2312a7a497ea7c5cfa4064c
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-30 19:05:18 -0400

    wip with devise

--- Gemfile
+++ Gemfile
@@ -30,7 +30,8 @@ gem 'gmaps4rails'
 
 
 # Use ActiveModel has_secure_password
- gem 'bcrypt', '~> 3.1.7'
+ # gem 'bcrypt', '~> 3.1.7'
+ gem 'devise'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525231439) do
+ActiveRecord::Schema.define(version: 20150524200736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +56,8 @@ ActiveRecord::Schema.define(version: 20150525231439) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
