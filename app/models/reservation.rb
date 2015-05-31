06714a16bb4458c86f784726e68fb099b4337ca6
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-31 17:30:34 -0400

    fixed reservation model validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,13 +8,13 @@ class Reservation < ActiveRecord::Base
   def capacity_size
   	existing_reservations = Reservation.where("restaurant_id = ? AND reserved_at > ? AND reserved_at < ?", self.restaurant_id, self.reserved_at.beginning_of_hour, self.reserved_at.end_of_hour)
 		total_seats_reserved = existing_reservations.sum(:party_size)
-		if party_size > (self.restaurant.capacity - total_seats_reserved)
+		if party_size.to_i > (self.restaurant.capacity - total_seats_reserved)
 			errors[:base] << "Capacity Exceeded"
 		end
   end 
 
   def reservation_between_six_and_ten
-  	if reserved_at.hour < 18 or reserved_at.hour > 20 
+  	if reserved_at.hour < 18 or reserved_at.hour > 21 
   		errors[:base] << "Reservation hours has to be between 6 PM and 10 PM"
   	end
   	
