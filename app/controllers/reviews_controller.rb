4c6241f2ef2d7327af27faaa7416d655cf0a1470
Rishab Chadha <chadha.rishab@gmail.com>
2015-06-21 16:56:49 -0400

    added ajax for reviews on restaurant#show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,18 +6,13 @@ class ReviewsController < ApplicationController
 		@review.comment = params[:review][:comment]
 		@review.restaurant_id = @restaurant.id
 
-		if @review.save
-			redirect_to restaurant_path(@restaurant)
-		else
-			render "/restaurants/show"
-		end 
+		@review.save
+
+		# if @review.save
+		# 	redirect_to restaurant_path(@restaurant)
+		# else
+		# 	render "/restaurants/show"
+		# end 
 
 	end
-
-	private
-
-	def sanitized_review_params
-		params.require(:review).permit(:comment, :restaurant_id)
-	end
-
 end
