7700989a78e9f2b48c4172b4018ceebf15719d50
Rishab Chadha <chadha.rishab@gmail.com>
2015-06-29 19:21:59 -0400

    deleted custine_resturant migration

--- db/migrate/20150516184119_drop_cusine_resturants_table.rb
+++
@@ -1,9 +0,0 @@
-class DropCusineResturantsTable < ActiveRecord::Migration
-  def up
-    drop_table :cuisine_restaurants
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
